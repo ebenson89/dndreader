03d31187900606b79809e39d594327f02e9917e0
ebenson89 <embox4@gmail.com>
2021-05-07 11:57:16 -0500

    Fixed comments

--- diceroller.py
+++ diceroller.py
@@ -25,67 +25,65 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.random_item_button.clicked.connect(self.random_entry)
         self.files_list_output.clicked.connect(self.choose_file)
 
-    #Choose a file from the data folder
     def choose_file(self, qmodelindex):
+        """Choose a file from the data folder"""
         global json_file_path
         item = self.files_list_output.currentItem()
         json_file_path = start_folder + item.text()
     
-    #Pull data from file
     def get_data(self, file_name):
-        #Open json file
+        """Pull data from file"""
+        # Open json file
         with open(file_name) as data_dict:
-            #Return json obj as a dict
+            # Return json obj as a dict
             return json.load(data_dict)
 
-    #Roll how_manyDkind of dice
     def roll_dice(self):
-        #User Input
+        """Roll how_many d kind of dice"""
+        # User Input
         how_many = self.how_many_box.text()
         kind = self.kind_box.text()
 
-        #Check if user input present
+        # Check if user input present
         if how_many == "" or kind == "":
             self.results_output.setText("Please Enter dice")
         else:
-            #Total variables
+            # Total variables
             total = 0
             output_str = how_many + "d" + kind + ": "
             
-            #Roll the choosen dice how_many times
+            # Roll the choosen dice how_many times
             for x in range (1, int(how_many) + 1):
-                #Pick a random number
+                # Pick a random number
                 number = random.randint(1, int(kind))
-                #Add number to the total
+                # Add number to the total
                 total += number
-                #Add the number to the output string
+                # Add the number to the output string
                 output_str = output_str + (str(number))
-                #Add addition symbols between the numbers in the string
+                # Add addition symbols between the numbers in the string
                 if 0 < x < int(how_many):
                     output_str = output_str + (" + ")
 
-            #Add on the total to the output string
+            # Add on the total to the output string
             output_str = output_str + (" = ") + str(total)
-            #Print out string
+            # Print out string
             self.results_output.setText(output_str)
 
-    #Choose a random item from the file
     def random_entry(self):
-
-        #Check if user has selected a file
+        """Choose a random item from the file"""
+        # Check if user has selected a file
         if json_file_path == "":
             self.random_item_output.setText("Please choose a file.")
         else:
-            #Grab the data from the choosen file
+            # Grab the data from the choosen file
             data_dict = self.get_data(json_file_path)
-            #Randomly choose a key
+            # Randomly choose a key
             key = random.choice(list(data_dict.keys()))
-            #Print out key and value in a string
+            # Print out key and value in a string
             self.random_item_output.setText(key + ": " + data_dict[key])
 
-    #Choose a file in the database
     def files_in_data_folder(self):
-
+        """Choose a file in the database"""
         for _ , _ ,file_names in os.walk("datafiles"):
             if file_names[0][-5:] == ".json":
                 self.files_list_output.insertItems(0, file_names)
